96ee3dd6ceb7421fd3458ec1a482e0f0248b5c99
nobiaya <lilypromise99@gmail.com>
2024-10-05 15:00:53 +0100

    added temple page

---
+++ script/temples.js
@@ -0,0 +1,19 @@
+const mainnav = document.querySelector('.navigation');
+const hambutton = document.querySelector('#menu');
+const h1 = document.querySelector('h1');
+
+// This toggle menu visibility and button content
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+	hambutton.setAttribute('aria-expanded', hambutton.classList.contains('show') ? 'true' : 'false');
+	hambutton.textContent = hambutton.classList.contains('show') ? 'X' : '☰';
+});
+
+// This display current year in footer
+const currentyear = document.querySelector("#currentyear");
+currentyear.textContent = new Date().getFullYear();
+
+// This display last modified date
+const lastModifiedElement = document.getElementById('lastModified');
+lastModifiedElement.textContent = document.lastModified;
